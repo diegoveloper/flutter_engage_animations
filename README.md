146a0ab6ae94641dd4af8644c23ad8ac8247a0bf
diegoveloper <diego.velasquez.lopez@gmail.com>
2021-02-13 00:35:25 -0500

    readme updated

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # flutter_engage_animations
 
 Twitter: www.twitter.com/diegoveloper
+
 Youtube: www.youtube.com/diegoveloper
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -31,6 +31,12 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   }
 
   @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
